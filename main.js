b252a1c4578b40b914147788fae76c3d6dd30a78
Wupfai <987003803@qq.com>
2022-04-03 16:43:13 +0800

    33

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
-
 canvas.width=document.documentElement.clientWidth;
 canvas.height=document.documentElement.clientHeight;
-
-let painting = false
-canvas.onmousedown = ()=>{
-    painting = true
-}
-canvas.onmousemove=(e)=>{
-    if (painting===true){
+function drawLine(x1,x2,x3,x4) {
     ctx.beginPath();
-    ctx.arc(e.clientX,e.clientY,5,0,360);
-    ctx.fill();
+    ctx.moveTo(x1,x2);
+    ctx.lineTo(x3,x4);
+    ctx.stroke();
+    ctx.lineCap = "round";
+    ctx.lineWidth=8;
+}
+let last;
+let painting = false;
+ctx.fillStyle = "black";
+let isTouchDevice = "ontouchstart" in  document.documentElement;
+if (isTouchDevice){
+    canvas.ontouchstart=(e)=>{
+        last = [e.touches[0].clientX,e.touches[0].clientY];
     }
-
-    ctx.fillStyle = "black"
+    canvas.ontouchmove=(e)=>{
+        let x = e.touches[0].clientX;
+        let y = e.touches[0].clientY;
+        drawLine(last[0],last[1],x,y);
+        last =[x,y];
+    }
+}else{
+    canvas.onmousedown = (e)=>{
+        painting = true;
+        last=[e.clientX,e.clientY]
+    }
+    canvas.onmousemove=(e)=>{
+        if (painting===true){
+            ctx.beginPath();
+            drawLine(last[0],last[1],e.clientX,e.clientY);
+            last =[e.clientX,e.clientY];
+        }else{
+            console.log("什么也没做")
+        }
+    }
+    canvas.onmouseup = ()=>{
+        painting=false
+    }
 }
-canvas.onmouseup = ()=>{
-    painting=false
-}
 
 
 
 
-// canvas.onmousemove = (e)=>{
-//     let div2 = document.createElement("div")
-//     div2.style.position="absolute"
-//     div2.style.left=e.clientX+"px"
-//     div2.style.top=e.clientY+"px"
-//     div2.style.background="black"
-//     div2.style.borderRadius="50%"
-//     div2.style.width="6px"
-//     div2.style.height="6px"
-//     div2.style.marginLeft="-3px"
-//     div2.style.marginTop="-3px"
-//
-//     canvas.appendChild(div2)
-// }
